a2d39d4c55e86595e335c0e3919b6253473867ca
Sümeyye Rana <74724438+ranauzel@users.noreply.github.com>
2022-10-14 13:53:00 +0300

    Add files via upload

---
+++ indeks.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron')
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 600,
+      height: 700,
+      frame: false
+    })
+  
+    win.loadFile('mp3.html')
+  }
+  app.whenReady().then(() =>{
+      createWindow()
+  })
+
